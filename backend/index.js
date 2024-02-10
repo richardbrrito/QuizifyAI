d942ca868168ace323a71c3ad6703c843ec8a532
Eric Gessa <eric.gessa@gmail.com>
2024-02-10 11:44:28 -0500

    add generateQuestions

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express, { urlencoded } from 'express';
 import { answerQuestion } from './answering.js'
+import { generateQuestions } from './pdfQA.js';
 
 const app = express();
 
@@ -24,6 +25,24 @@ app.post('/answer', async (req, res) => {
         res.status(500).send({ error: 'Something went wrong' });
     }
 });
+
+app.post('/generate', (req, res) => {
+    const pdfPath = req.body.pdfPath;
+    const startPage = Number(req.body.startPage);
+    const endPage = Number(req.body.endPage);
+
+    if (!pdfPath) {
+        return res.status(400).send({ error: 'PDF is required' });
+    }
+
+    try {
+        const questions = generateQuestions(pdfPath, startPage, endPage);
+        res.send(questions);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send({ error: 'Something went wrong' })
+    }
+})
 
 app.listen(3000, () => {
     console.log('App is listening on port 3000');
