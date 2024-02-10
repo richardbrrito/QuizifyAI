7beb1f878c4b2866f237019404e23551a809c0b4
Kevin Camacho <Kevincxv@outlook.com>
2024-02-10 10:09:28 -0500

    Meta Data

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Quizmify",
+    page_icon="favicon.png",
+    layout="centered",
+    initial_sidebar_state="expanded",
+    menu_items={
+        
+    }
+)
