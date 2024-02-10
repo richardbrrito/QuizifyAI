7417fac7a6e4c83f864ee17c3bd2d473090bbedd
Richard Brito <richardbrito116@gmail.com>
2024-02-10 12:33:55 -0500

    made options into array, made seperate page depending on option
    made options into array, made seperate page depending on option

--- main.py
+++ main.py
@@ -66,13 +66,17 @@ def nav_page(page_name, timeout_secs=3):
     """ % (page_name, timeout_secs)
     html(nav_script)
 
+
+
 st.header("Welcome to :green[Quizmify] your AI Learning Companion", help=None, divider=False)
 
 url = st.text_input("Enter a URL you would like to know more about!", value="URL", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False, label_visibility="visible")
 
 uploaded_file = st.file_uploader("Uplaod Your PDF Documents Here!", type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
 
-st.selectbox("Select what type of questions you would like to be asked!", ("Multiple Choice Quesions", "Free Response Questions"), index=0, key=None, help=None, on_change=None, args=None, kwargs=None, placeholder="Choose an option", disabled=False, label_visibility="visible")
+
+
+option = st.selectbox("Select what type of questions you would like to be asked!", (["Multiple Choice Questions", "Free Response Questions"]), index=0, key=None, help=None, on_change=None, args=None, kwargs=None, placeholder="Choose an option", disabled=False, label_visibility="visible")
 
 num_questions = st.number_input("How man questions do you want?", min_value=1, max_value=20, value="min", step=None, format=None, key=None, help=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False, label_visibility="visible")
 
@@ -90,5 +94,10 @@ if st.button("Generate Quiz"):
 
         st.write("Quiz is ready!")
         nav_page("questions")
+        if 'Free Response Questions' in option:
+            nav_page("frquestion")
+        if 'Multiple Choice Questions' in option:
+            nav_page("mcquestion")
+            
     else:
         st.error("Please make sure all inputs are provided correctly.")
