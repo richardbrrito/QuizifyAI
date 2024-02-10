3d51419e8f4302abdf0333a5bc3a715158e63628
Richard Brito <richardbrito116@gmail.com>
2024-02-10 12:05:15 -0500

    made def nav_page, hid sidenav on questions, and hid button to bring it out
    made def nav_page, hid sidenav on questions, and hid button to bring it out

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit.components.v1 import html
 import time
 
 
@@ -40,6 +41,31 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
+def nav_page(page_name, timeout_secs=3):
+    nav_script = """
+        <script type="text/javascript">
+            function attempt_nav_page(page_name, start_time, timeout_secs) {
+                var links = window.parent.document.getElementsByTagName("a");
+                for (var i = 0; i < links.length; i++) {
+                    if (links[i].href.toLowerCase().endsWith("/" + page_name.toLowerCase())) {
+                        links[i].click();
+                        return;
+                    }
+                }
+                var elasped = new Date() - start_time;
+                if (elasped < timeout_secs * 1000) {
+                    setTimeout(attempt_nav_page, 100, page_name, start_time, timeout_secs);
+                } else {
+                    alert("Unable to navigate to page '" + page_name + "' after " + timeout_secs + " second(s).");
+                }
+            }
+            window.addEventListener("load", function() {
+                attempt_nav_page("%s", new Date(), %d);
+            });
+        </script>
+    """ % (page_name, timeout_secs)
+    html(nav_script)
+
 st.header("Welcome to :green[Quizmify] your AI Learning Companion", help=None, divider=False)
 
 url = st.text_input("Enter a URL you would like to know more about!", value="URL", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False, label_visibility="visible")
@@ -63,6 +89,7 @@ if st.button("Generate Quiz"):
         progress_bar.empty()
 
         st.write("Quiz is ready!")
+        nav_page("questions")
     else:
         st.error("Please make sure all inputs are provided correctly.")
 
